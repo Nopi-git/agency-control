a3d6e72e6ed569ababa0c45ee6ddce7b8775ca9a
Nopi <nopiwork@gmail.com>
2019-07-31 19:59:39 +0200

    Add basic react app

---
+++ desktop-site/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        Control
+      </header>
+    </div>
+  );
+}
+
+export default App;
